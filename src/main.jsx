ca897689418d89a9b2552c89771cb210d11bba00
Creatlydev <creatlydev@gmail.com>
2024-06-30 12:51:04 -0500

    Add Palette colors from colors file

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,8 @@ import './styles/index.css'
 import { App } from './App.jsx'
 import { CssBaseline, ThemeProvider, createTheme } from '@mui/material';
 
+import { PalleteColors } from '../colors.js';
+
 
 //import fuente roboto
 import "@fontsource/roboto/300.css";
@@ -12,18 +14,8 @@ import "@fontsource/roboto/500.css";
 import "@fontsource/roboto/700.css";
 
 const theme = createTheme({
-  palette: {
-    mode: 'dark',
-    primary: {
-      main: '#0BA884',
-    },
-    secondary: {
-      main: '#6A747B',
-    },
-    dark: {
-      main: '#132026'
-    }
-  },
+  palette: PalleteColors
+
 });
 
 ReactDOM.createRoot(document.getElementById('root')).render(
