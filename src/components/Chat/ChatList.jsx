0eed838384e5ff776f0703d40bc04522dca59d2d
Creatlydev <creatlydev@gmail.com>
2024-06-30 13:02:12 -0500

    Remove import react-icons

--- src/components/Chat/ChatList.jsx
+++ src/components/Chat/ChatList.jsx
@@ -1,4 +1,4 @@
-import { BsCheckAll } from "react-icons/bs";
+import DoneAllOutlinedIcon from '@mui/icons-material/DoneAllOutlined';
 import List from "@mui/material/List";
 import ListItem from "@mui/material/ListItem";
 import Avatar from "@mui/material/Avatar";
@@ -6,7 +6,7 @@ import Stack from "@mui/material/Stack";
 
 const ChatList = ({ list }) => {
   return (
-    <List className="bg-clr-bg">
+    <List className="bg-blue-dark">
       {list.map((chat, index) => (
         <ListItem key={index}>
           <div className="flex w-full gap-2 relative py-2">
@@ -15,14 +15,14 @@ const ChatList = ({ list }) => {
             </Stack>
             <div>
               <div className="flex justify-between">
-                <h3 className="text-white font-medium">{chat.nameUser}</h3>
-                <span className="absolute right-0 text-clr-secondary text-sm">
+                <h3 className="text-grey-light font-medium">{chat.nameUser}</h3>
+                <span className="absolute right-0 text-grey-medium text-sm">
                   {chat.LastTime_Message}
                 </span>
               </div>
               <div className="flex items-center">
-                <BsCheckAll className={chat.unread ? "text-clr-secondary" : "text-read-message-icon"} size={"1.5rem"} />
-                <span className="text-clr-secondary">
+                <DoneAllOutlinedIcon className={chat.unread ? "text-grey-medium" : "text-blue-main"} size={"1.5rem"} />
+                <span className="text-grey-medium">
                   {chat.LastContent_Message}
                 </span>
               </div>
