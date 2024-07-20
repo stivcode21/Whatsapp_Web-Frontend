11d4131bf91a70b9261e9f05f4d9e6848097d253
Segura Mendoza Gael Alejandro <gaelsegura.mendoza@gmail.com>
2024-07-20 12:59:34 -0600

    Add Icons into ChatMenu component

--- src/components/Chats/ChatMenu.jsx
+++ src/components/Chats/ChatMenu.jsx
@@ -1,7 +1,26 @@
+import { ChatOutlined, Groups, SettingsOutlined } from "@mui/icons-material"
+import Button from "../Common/Button"
+import { Avatar } from "@mui/material"
+
 export default function ChatMenu() {
   return(
-    <div className="bg-blue-dark w-[60px] border-r-[1px] border-grey-main">
-
+    <div className="bg-blue-dark w-[60px] border-r-[1px] border-grey-main flex flex-col justify-between  items-center py-6 text-grey-light">
+      <div className="flex flex-col gap-4">
+        <Button>
+          <ChatOutlined />
+        </Button>
+        <Button>
+          <Groups />
+        </Button>
+      </div>
+      <div className="flex flex-col gap-4">
+        <Button>
+          <SettingsOutlined />
+        </Button>
+        <Button>
+          <Avatar />
+        </Button>
+      </div>
     </div>
   )
 }
