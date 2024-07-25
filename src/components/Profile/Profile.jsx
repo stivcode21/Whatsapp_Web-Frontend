d84188f50a7d7ead4f82467f59b11e4892da5b88
GaelSM <gaelsegura.mendoza@gmail.com>
2024-07-25 12:16:14 -0600

    Add Input component, and create useFormProfile hook to separate logic

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,25 +1,38 @@
 import { Avatar } from "@mui/material";
+import useFormProfile from "../../hooks/useFormProfile";
+import Input from "./Input";
 
 export default function Profile() {
+  const {profileData, formRef, handleEdit } = useFormProfile()
+
   return (
-  <>
-    <header className="pl-[25px] pr-[20px] h-[60px] flex items-center">
-      <h1 className="text-white font-semibold text-2xl"> Perfil </h1>
-    </header>
-    <div className="my-[28px] flex justify-center">
-      <Avatar sx={{ width: 200, height: 200 }} src="../../assets/McLovin.jpg"/>
-    </div>
-    <div className="px-[30px] pt-[14px] pb-[10px] mb-[10px] font-light">
-      <p className="mb-[14px] text-green-main text-sm">Tu nombre</p>
-      <p className="text-white">Mc Lovin</p>
-    </div>
-    <div className="px-[30px] text-sm text-grey-medium font-light">
-      <p>Este no es tu nombre de usuario o PIN. Este nombre será visible para tus contactos de WhatsApp.</p>
-    </div>
-    <div className="px-[30px] pt-[14px] pb-[10px] mb-[10px] font-light">
-      <p className="mb-[14px] text-green-main text-sm">Info</p>
-      <p className="text-white">A eso le llamo estrategia</p>
-    </div>
-  </>
+    <>
+      <header className="pl-[25px] pr-[20px] h-[60px] flex items-center">
+        <h1 className="text-white font-semibold text-2xl"> Perfil </h1>
+      </header>
+      <div className="my-[28px] flex justify-center">
+        <Avatar
+          sx={{ width: 200, height: 200 }}
+          src="../../assets/McLovin.jpg"
+        />
+      </div>
+      <form onSubmit={(e) => e.preventDefault()} ref={formRef}>
+        <Input
+          text="Tu nombre"
+          defaultValue={profileData.name}
+          name="name"
+          onEdit={handleEdit}
+        />
+        <div className="px-[30px] text-sm text-grey-medium font-light">
+          <p>Este no es tu nombre de usuario o PIN. Este nombre será visible para tus contactos de WhatsApp.</p>
+        </div>
+        <Input
+          text="Info."
+          defaultValue={profileData.description}
+          name="description"
+          onEdit={handleEdit}
+        />
+      </form>
+    </>
   )
 }
